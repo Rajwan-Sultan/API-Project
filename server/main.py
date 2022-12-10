cc712d904691692d01d956dae482b77d904380fc
sultan0041 <111181255+sultan0041@users.noreply.github.com>
2022-12-10 04:07:41 -0500

    Update main.py

--- server/main.py
+++ server/main.py
@@ -41,47 +41,48 @@ try:
                     call_file(object_path.split("\\")[-1])
                 except Exception as e:
                     print("Error in other python file", e)
-            else:
-                for item in postfix:
-                    filename = prefix+'_'+str(item)+'.'+postfix2
+
+            for item in postfix:
+                filename = prefix+'_'+str(item)+'.'+postfix2
+                if postfix2 == 'txt':
                     print(filename)
-                    # shutil.copy(object_path, f"{destination_path}\{filename}")#! sends the new file to the destination
-                    # print(myfilename)##../destination/\some_1.txt
+                # shutil.copy(object_path, f"{destination_path}\{filename}")#! sends the new file to the destination
+                # print(myfilename)##../destination/\some_1.txt
 
-                    #! creats new file in the server
-                    shutil.copy(object_path, filename)
-                    myfilename = filename
-                    lines = ""
-                    myfile = open(myfilename, 'r')
-                    myline = myfile.readlines()  # gives a list of lines
-                    for i in myline:
-                        lines += i
-                    myfile.close()
-                    # print(lines)
-                    with open(filename, 'a') as f:
-                        i = myfilename.split("_")
-                        j = i[1].split(".")
-                        f.write(('\n'+lines)*(int(j[0])*10-1))
-                    f.close()
+                #! creats new file in the server
+                shutil.copy(object_path, filename)
+                myfilename = filename
+                lines = ""
+                myfile = open(myfilename, 'r')
+                myline = myfile.readlines()  # gives a list of lines
+                for i in myline:
+                    lines += i
+                myfile.close()
+                # print(lines)
+                with open(filename, 'a') as f:
+                    i = myfilename.split("_")
+                    j = i[1].split(".")
+                    f.write(('\n'+lines)*(int(j[0])*10-1))
+                f.close()
 
-                    zipO = zipfile.ZipFile("All.zip", "w")
-                    for i in os.listdir():
-                        if i.endswith(".txt") and i != prefix+".txt":
-                            zipO.write(i)
-                    zipO.close()
-                    shutil.copy(f"../server/All.zip",
-                                f"{destination_path}All.zip")
-                    os.remove("All.zip")
+                zipO = zipfile.ZipFile("All.zip", "w")
+                for i in os.listdir():
+                    if i.endswith(".txt") and i != prefix+".txt":
+                        zipO.write(i)
+                zipO.close()
+                shutil.copy(f"../server/All.zip",
+                            f"{destination_path}All.zip")
+                os.remove("All.zip")
 
-            os.remove(object_path)
-            os.remove(object_path.split("\\")[-1])
-            for i in os.listdir():
-                if i.endswith(".txt"):
-                    os.remove(i)
-            os.chdir(destination_path)
-            shutil.unpack_archive("All.zip", destination_path)
-            os.remove("All.zip")
-            os.chdir("../server")
+        os.remove(object_path)
+        os.remove(object_path.split("\\")[-1])
+        for i in os.listdir():
+            if i != "main.py":
+                os.remove(i)
+        os.chdir(destination_path)
+        shutil.unpack_archive("All.zip", destination_path)
+        os.remove("All.zip")
+        os.chdir("../server")
 
 except:
     print("Exiting from command")
